68d5abe9aa9a89985e74f897ffe237a39de79cb0
danielleroux <danielleroux@users.noreply.github.com>
2022-10-14 09:21:57 +0000

    deploy: siemens/ix@b9edc6829fcb005e8ffb7890fbc52e11e5edf79b

--- webcomponent-examples/dist/assets/ix-modal_2.entry.87f53bb8.js
+++
@@ -1,9 +0,0 @@
-import { M, a } from "./modal-f9b3f7a9.523ff0b2.js";
-import "./index.dc1a0ae3.js";
-import "./anime.es-185e9087.87a18bcc.js";
-import "./animation-b667a4c4.18fe34bf.js";
-import "./typed-event-dd6c83dd.77656892.js";
-export {
-  M as ix_modal,
-  a as ix_modal_container
-};
