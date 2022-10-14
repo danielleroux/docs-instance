78723d760c20756bf12661a50b3aeeb4fff57646
danielleroux <danielleroux@users.noreply.github.com>
2022-10-14 08:51:20 +0000

    deploy: siemens/ix@82763d8515820d7dcbb08c36b819d0b38048bac7

---
+++ webcomponent-examples/dist/assets/toast-custom.58760148.js
@@ -0,0 +1,25 @@
+import "./index.dc1a0ae3.js";
+import { t as toast } from "./index.2eef27b6.js";
+import "./init.e920e8e8.js";
+import "./logical-filter-operator-1bf83315.1b3b3530.js";
+import "./flip-tile-state-28a1f8ce.23318fa9.js";
+import "./modal-f9b3f7a9.523ff0b2.js";
+import "./typed-event-dd6c83dd.77656892.js";
+import "./anime.es-185e9087.87a18bcc.js";
+import "./animation-b667a4c4.18fe34bf.js";
+import "./upload-file-state-631bb8a2.5434751f.js";
+(async function() {
+  await window.customElements.whenDefined("ix-toast-container");
+  document.getElementById("toastButton").addEventListener("click", async () => {
+    const customModal = document.createElement("div");
+    const template = document.getElementById("custom-toast");
+    const templateContent = template.content;
+    customModal.appendChild(templateContent);
+    const instance = await toast({
+      message: customModal
+    });
+    customModal.querySelector("ix-button").addEventListener("click", () => {
+      instance.close();
+    });
+  });
+})();
